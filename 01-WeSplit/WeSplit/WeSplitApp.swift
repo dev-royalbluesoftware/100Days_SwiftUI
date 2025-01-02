cd644484eac9a3ca3a91edd753935ab2276ba250
rbs-dev <111300320+dev-royalbluesoftware@users.noreply.github.com>
2025-01-01 20:37:17 -0700

    Project 1 initial setup

---
+++ 01-WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,20 @@
+//
+//
+// WeSplit
+// WeSplitApp.swift
+//
+// Created by rbs-dev
+// Copyright © Royal Blue Software
+// 
+
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
