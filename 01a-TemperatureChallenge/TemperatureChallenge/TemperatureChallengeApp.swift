d63365cc7f0a0fa0cfe3370db38fe9f1894f7de4
rbs-dev <111300320+dev-royalbluesoftware@users.noreply.github.com>
2025-01-07 22:41:54 -0700

    Temperature Challenge project created

---
+++ 01a-TemperatureChallenge/TemperatureChallenge/TemperatureChallengeApp.swift
@@ -0,0 +1,20 @@
+//
+//
+// TemperatureChallenge
+// TemperatureChallengeApp.swift
+//
+// Created by rbs-dev
+// Copyright © Royal Blue Software
+// 
+
+
+import SwiftUI
+
+@main
+struct TemperatureChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
