6e40f90ca49ac623a4b5d3e31388fecf66353601
rbs-dev <111300320+dev-royalbluesoftware@users.noreply.github.com>
2025-01-08 22:55:29 -0700

    Project 3 Views and Modifiers initial setup

---
+++ 03-ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,20 @@
+//
+//
+// ViewsAndModifiers
+// ViewsAndModifiersApp.swift
+//
+// Created by rbs-dev
+// Copyright © Royal Blue Software
+// 
+
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
