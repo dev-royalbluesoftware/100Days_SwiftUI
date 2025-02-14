7611a40ed9b72eb7216b32943e654f4f3d375aa0
rbs-dev <111300320+dev-royalbluesoftware@users.noreply.github.com>
2025-02-14 15:11:20 -0700

    Guess the Flag initial commit

---
+++ 02_GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,20 @@
+//
+//
+// GuessTheFlag
+// GuessTheFlagApp.swift
+//
+// Created by rbs-dev
+// Copyright © Royal Blue Software
+// 
+
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
